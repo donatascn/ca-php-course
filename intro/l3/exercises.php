93f534bd0e5634db60367d084dc6e43e202fce37
donatas <donatas.cinikas@gmail.com>
2022-07-12 21:24:46 +0300

    improve exercises

--- intro/l3/exercises.php
+++ intro/l3/exercises.php
@@ -183,3 +183,18 @@ kuris leistų nesunkiai spausdinti daugiau eilučių
 1   5   10   10   5   1
 ...
 */
+
+/*
+16. Išspausdinkite šią struktūrą. $n gali keistis
+$n = 5;
+
+5 5 5 5 5 5 5 5 5
+5 4 4 4 4 4 4 4 5
+5 4 3 3 3 3 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 2 1 2 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 3 3 3 3 4 5
+5 4 4 4 4 4 4 4 5
+5 5 5 5 5 5 5 5 5
+*/
