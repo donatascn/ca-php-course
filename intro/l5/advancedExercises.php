7a4774c8b370ed3c69409e29c1c53cec7520d9aa
donatas <donatas.cinikas@gmail.com>
2022-07-21 18:58:12 +0300

    improve exercises

--- intro/l5/advancedExercises.php
+++ intro/l5/advancedExercises.php
@@ -14,14 +14,16 @@ function exercise1(): array
 
     /*
     Sunaikinkitę visus elementus, kurių reikšmė yra 'error' ir grąžinkite pamodifikuotą masyvą.
-    Tikėkitės, kad $products masyvas gali turėti ne 5, 100 elementų - naudokite ciklą.
+    Tikėkitės, kad $products masyvo narių skaičius gali varijuoti, tad naudokite ciklą.
     */
 
     return [];
 }
 
-function exercise2(int $keyPart)
+function exercise2(int $id)
 {
+    $prefix = 'product';
+
     $products = [
         'product_1' => 'desk',
         'product_2' => 'lamp',
@@ -29,8 +31,9 @@ function exercise2(int $keyPart)
     ];
 
     /*
-    Sunaikinkitę reikšmę, kuri atitiktų raktą 'product_' + $keyPart ir grąžinkite pamodifikuotą masyvą.
-    Jeigu tokio rakto nėra, gražinkite null. Pridėkite trūkstamą return tipą.
+    Sunaikinkitę visas masyve esančias reikšmes, kurių raktai yra $prefix + $id. Grąžinkite pamodifikuotą masyvą.
+    Jeigu tokio rakto nėra, gražinkite null.
+    Pridėkite trūkstamą return tipą.
     Funkcijos kvietimas: exercise2(1)
     */
 
@@ -51,14 +54,14 @@ function exercise3(): array
     ];
 
     /*
-    Sunaikinkitę visus elementus, kurių reikšmė yra 'error' ir grąžinkite pamodifikuotą masyvą.
-    Tikėkitės, kad $products masyvas gali turėti ne 5, 100 elementų - naudokite ciklą.
+    Sunaikinkite visus elementus, kurių 'status' reikšmė yra 'error' ir grąžinkite pamodifikuotą masyvą.
+    Tikėkitės, kad $transactions masyvo narių skaičius gali varijuoti, tad naudokite ciklą.
     */
 
     return [];
 }
 
-function exercise4(string $key): string
+function exercise4(string $key)
 {
     $products = [
         'product_1' => 'shirt',
@@ -66,15 +69,87 @@ function exercise4(string $key): string
         'product_98' => 'coat',
     ];
     /*
-    Funkcija turi grąžinti reikšmę pagal paduota raktą.
-    Jeigu paduotas raktas neegzistuoja $products masyve, gražinkite tekstą 'Item not found'.
+    Funkcija turi grąžinti reikšmę pagal paduotą raktą.
+    Jeigu paduotas raktas neegzistuoja $products masyve, grąžinkite null.
+    Pridėkite teisingą return tipą.
     Funkcijos kvietimas: exercise4('product_2')
     */
+}
 
-    return '';
+// Žr. kitą užduotį
+function getProducts(): array
+{
+    return [
+        'chair' => [
+            'type' => 'furniture',
+            'name' => 'Best chair',
+            'price' => 15,
+        ],
+        'lamp' => [
+            'type' => 'lighting',
+            'name' => 'Ultimate lamp',
+            'price' => 99,
+        ],
+        'sofa' => [
+            'type' => 'furniture',
+            'name' => 'Soft sofa',
+            'price' => 350
+        ],
+    ];
 }
 
 function exercise5(): array
+{
+    $products = getProducts();
+    $fridge = [
+        'type' => 'appliance',
+        'name' => 'Coolest fridge',
+        'price' => 200,
+    ];
+    /*
+    Į masyvą $products pridėkite naują narį ir grąžinkite naujajį masyvą. Nario 'key' - 'fridge'.
+    Nario reikšmė - $fridge
+    */
+
+    return [];
+}
+
+function exercise6(): int
+{
+    $products = getProducts();
+    /*
+    Raskite ir grąžinkite visų produktų kainų vidurkį
+    */
+
+    return 0;
+}
+
+function exercise7(): array
+{
+    $products = getProducts();
+    /*
+    Sudėkite visų produktų pavadinimus į masyvą ir jį grąžinkite
+    [
+        'Best chair',
+        'Ultimate lamp',
+        'Soft sofa',
+    ]
+    */
+
+    return [];
+}
+
+function exercise8(): void
+{
+    $products = getProducts();
+    /*
+    Iteruodami per masyvą išspausdinkite eilutę, kurioje matytusi produkto pavadinimas ir tipas atskirti brūkšneliu:
+    Best chair - furniture, Ultimate lamp - lighting, Soft sofa - furniture
+    */
+
+}
+
+function exercise9(): array
 {
     $transactions = [
         [
@@ -89,7 +164,7 @@ function exercise5(): array
     /*
     Kiekvienai iš transakcijų, esančių kintamajame $transactions, suskaičiuokite galutinę sumą ir pridėkite į
     transakciją su raktu 'total'. Grąžinkite $transactions iš funkcijos.
-    Tikėkitės, kad transakciju skaičius gali išaugti. Jų gali būti ne 2, o 100. Dėl to naudokite ciklą.
+    Tikėkitės, kad transakcijų skaičius gali išaugti. Jų gali būti ne 2, o 100. Dėl to naudokite ciklą.
 
     Laukiamas rezultatas:
     [
@@ -105,53 +180,10 @@ function exercise5(): array
     return [];
 }
 
-function exercise6(): array
-{
-    $currencyRates = [
-        'usd' => 1.13,
-        'gbp' => 0.83,
-    ];
-
-    $transactions = [
-        [
-            'count' => 2,
-            'price' => 3.55
-        ],
-        [
-            'count' => 15,
-            'price' => 14.1
-        ],
-    ];
-    /*
-    Kiekvienai iš transakcijų, esančių kintamajame $transactions, suskaičiuokite galutinę sumą visomis valiutomis
-    esančiomis kintamajame $currencyRates (taip pat ir bazine valiuta - eur) ir pridėkite į transakciją su raktu 'totals'.
-    Apvalinkite dviejų skaitmenų po kablelio tikslumu.
-    Grąžinkite pamodifikuotą $transactions masyvą iš funkcijos. 
-    Tikėkitės, kad transakciju skaičius gali išaugti. Jų gali būti ne 2, o 100. Dėl to naudokite ciklą.
-    Valiutų skaičius taip pat gali augti.
-
-    Laukiamas rezultatas:
-    [
-        [
-            'count' => 2,
-            'price' => 3.55,
-            'totals' => [
-                'eur' => ...,
-                'usd' => ...,
-                'gbp' => ...,
-            ],
-        ],
-        ...
-    ];
-    */
-
-    return [];
-}
-
-function exercise7(array $collection): array
+function exercise10(array $collection): array
 {
     /*
-    Funkcijai paduodama produktų kolekcija:
+    Funkcijai paduodama produktų masyvas:
     $productCollection = [
         [
             'name' => 'Best sofa',
@@ -161,19 +193,20 @@ function exercise7(array $collection): array
     ];
     exercise7($productCollection);
 
-    Funkcija turi grąžinti performuota kolekciją - 'name' turi tapti kolekcijos elemento raktu:
+    Funkcija turi grąžinti performuotą masyvą - 'name' turi tapti to masyvo elemento raktu:
     [
         'Best sofa' => [
             'price' => '55,
         ],
         ...
     ]
+    Galite kurti naują masyvą arba modifikuoti esamą.
     */
 
     return [];
 }
 
-function exercise8(): array
+function exercise11(): array
 {
     $products = [
         'desk',
@@ -184,6 +217,7 @@ function exercise8(): array
 
     /*
     Išskaidykite produktų pavadinimus į raides.
+    Žodžio skaidymui į raides naudokite funkciją https://www.php.net/manual/en/function.str-split.php
     [
         'desk' => [
             'd',
@@ -198,7 +232,55 @@ function exercise8(): array
     return [];
 }
 
-function exercise9(): void
+function exercise12(): array
+{
+    $currencyRates = [
+        'usd' => 1.13,
+        'gbp' => 0.83,
+    ];
+
+    $transactions = [
+        [
+            'product_id' => 1555,
+            'count' => 2,
+            'price_eur' => 10
+        ],
+        [
+            'product_id' => 985,
+            'count' => 15,
+            'price_eur' => 15
+        ],
+    ];
+    /*
+    Kiekvienai iš transakcijų, esančių kintamajame $transactions, suskaičiuokite galutinę sumą visomis valiutomis
+    esančiomis kintamajame $currencyRates (taip pat ir bazine valiuta - eur) ir pridėkite į transakciją su raktu 'totals'.
+    Apvalinkite dviejų skaitmenų po kablelio tikslumu.
+    Formules:
+    bazine valiuta: total = count * price_eur
+    kita valiuta: total = count * price_eur * currency_rate
+    Grąžinkite pamodifikuotą $transactions masyvą iš funkcijos.
+    Tikėkitės, kad transakciju skaičius gali išaugti. Jų gali būti ne 2, o 100. Dėl to naudokite ciklą.
+    Valiutų skaičius taip pat gali augti.
+
+    Laukiamas rezultatas:
+    [
+        [
+            'count' => 2,
+            'price_eur' => 10,
+            'totals' => [
+                'eur' => ...,
+                'usd' => ...,
+                'gbp' => ...,
+            ],
+        ],
+        ...
+    ];
+    */
+
+    return [];
+}
+
+function exercise13(): void
 {
     $animals = [
         [
@@ -234,77 +316,6 @@ function exercise9(): void
     land: chimp dog cat
     water: shark hippo crocodile
     */
-}
-
-function getProducts(): array
-{
-    return [
-        'chair' => [
-            'type' => 'furniture',
-            'name' => 'Best chair',
-            'price' => 15,
-        ],
-        'lamp' => [
-            'type' => 'lighting',
-            'name' => 'Ultimate lamp',
-            'price' => 99,
-        ],
-        'sofa' => [
-            'type' => 'furniture',
-            'name' => 'Soft sofa',
-            'price' => 350
-        ],
-    ];
-}
-
-function exercise10(): array
-{
-    $products = getProducts();
-    /*
-    Į masyvą $products pridėkite naują narį ir grąžinkite naujajį masyvą. Nario 'key' - 'fridge'. Nario reikšmė:
-    [
-        'type' => 'appliance',
-        'name' => 'Coolest fridge',
-        'price' => 200,
-    ]
-    */
-
-    return [];
-}
-
-function exercise11(): int
-{
-    $products = getProducts();
-    /*
-    Raskite ir grąžinkite visų produktų kainų vidurkį
-    */
-
-    return 0;
-}
-
-function exercise12(): array
-{
-    $products = getProducts();
-    /*
-    Sudėkite visų produktų pavadinimus į masyvą ir jį grąžinkite
-    [
-        'Best chair',
-        'Ultimate lamp',
-        'Soft sofa',
-    ]
-    */
-
-    return [];
-}
-
-function exercise13(): void
-{
-    $products = getProducts();
-    /*
-    Iteruodami per masyvą išspausdinkite eilutę, kurioje matytusi produkto pavadinimas ir tipas atskirti brūkšneliu:
-    Best chair - furniture, Ultimate lamp - lighting, Soft sofa - furniture
-    */
-
 }
 
 /*
