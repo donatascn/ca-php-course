1b7b0dc3bc97b859542a58833da3d6aa16a122d2
donatas <donatas.cinikas@gmail.com>
2022-08-02 18:38:32 +0300

    improve exercises

--- intro/l8/regularExercises.php
+++ intro/l8/regularExercises.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+//declare(strict_types=1);
 
 /*
 1. Išspausdinkite šio momento datą pasinaudodami funkcija 'date'
@@ -15,7 +15,7 @@ function exercise1(): void
 */
 function exercise2(): void
 {
-    
+
 }
 
 
@@ -27,10 +27,12 @@ function exercise2(): void
 - 1970/3/1
 - savaites numeris (52 savaitės metuose)
 - dienos numeris (365 dienos metuose)
+
+Naudokite objektą sukurtą kviečiant new DateTime()
 */
 function exercise3(): void
 {
-    
+
 }
 
 
@@ -42,11 +44,11 @@ function exercise3(): void
 */
 function exercise4(): void
 {
-    
+
 }
 
 /*
-5. Sukurkite datą iš '15th Jan 2021 8:15:01 PM' (data X). Pamodifikuokite, kad gautumėte:
+5. Sukurkite datą iš '15th Jan 2021 8:15:01 PM' (data X). Pamodifikuokite ją, kad gautumėte:
 - datą po 2 savaičių nuo datos X
 - datą po 10 metų nuo datos X
 - datą prieš 5 valandas nuo datos X
@@ -58,7 +60,7 @@ function exercise4(): void
 
 function exercise5(): void
 {
-    
+
 }
 
 function exercise6(): void
@@ -66,35 +68,37 @@ function exercise6(): void
     $products = [
         [
             'name' => 'Wine glass',
-            'last_purchase' => '2021 Jan 15 18:34:12',
+            'last_purchase_date' => '2021 Jan 15 18:34:12',
         ],
         [
             'name' => 'Bread knife',
-            'last_purchase' => '2020 Mar 15 23:14:00',
+            'last_purchase_date' => '2020 Mar 15 23:14:00',
         ],
         [
             'name' => 'Blue chair',
-            'last_purchase' => '2019 Dec 02 15:00:12',
+            'last_purchase_date' => '2019 Dec 02 15:00:12',
         ],
     ];
 
     /*
-    Išspausdinkite produktų paskutinių pirkimų santrauką:
+    Išspausdinkite produktų paskutinių pirkimų santrauką pasinaudodami $products masyvu.
+    Eilutėje turi spausdinti pavadinima (name) ir paskutinio pirkimo datą (last_purchase_date) pavyzdyje nurodytu formatu:
     Wine glass 2021-01-15 18:34:12
     ...
+    Blue chair 2019-12-02 15:00:12
     */
 }
 
-function exercise7($date1, $date2): string
+function exercise7(string $date1, string $date2): string
 {
     /*
     Palyginkite datas ir grąžinkite atsakymą, kuri data naujesnė.
 
-    Funkcijos kvietimas: exercise7(date_create('2022-01-25 17:13:25'), date_create('2020-01-25 17:13:25'));
+    Funkcijos kvietimas: exercise7('2022-01-25 17:13:25', '2020-01-25 17:13:25');
     Rezultatas:
     'First date is newer'
 
-    Funkcijos kvietimas: exercise7(date_create('2020-01-25 17:13:25'), date_create('2022-01-25 17:13:25'));
+    Funkcijos kvietimas: exercise7('2020-01-25 17:13:25', '2022-01-25 17:13:25');
     Rezultatas:
     'Second date is newer'
     */
@@ -102,18 +106,18 @@ function exercise7($date1, $date2): string
     return '';
 }
 
-function exercise8($date): void
+function exercise8(string $date): void
 {
     /*
     Išspausdinkite paduotos datos skirtumą nuo dabartinio momento žodžiais.
 
-    Funkcijos kvietimas: exercise8(date_create('2020-01-25 17:13:25'));
+    Funkcijos kvietimas: exercise8('2020-01-25 17:13:25');
     Rezultatas:
     Supplied date was 773 days ago
 
-    Funkcijos kvietimas: exercise8(date_create('2023-01-25 17:13:25'));
+    Funkcijos kvietimas: exercise8('2023-01-25 17:13:25');
     Rezultatas:
-    Supplied date is in the future
+    Supplied date is 500 days in the future
     */
 }
 
@@ -122,12 +126,12 @@ function exercise9($date): void
     /*
     Išspausdinkite datų skirtumą žodžiais.
 
-    Funkcijos kvietimas: exercise9(date_create('2020-01-25 17:13:25'));
+    Funkcijos kvietimas: exercise9('2020-01-25 17:13:25');
     Rezultatas:
-    Supplied date was 2 years 1 months 11 days
+    Supplied date was 2 years 1 months 11 days ago
 
-    Funkcijos kvietimas: exercise9(date_create('2023-01-25 17:13:25'));
+    Funkcijos kvietimas: exercise9('2023-01-25 17:13:25');
     Rezultatas:
-    Supplied date is in the future
+    Supplied date is 0 years 5 months 11 days in the future
     */
 }
