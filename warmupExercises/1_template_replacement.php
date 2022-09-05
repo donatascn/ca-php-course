ff4a2145a18dee09f55c0c4d71d15136504288cb
donatas <donatas.cinikas@gmail.com>
2022-09-05 18:30:16 +0300

    improve exercises

--- warmupExercises/1_template_replacement.php
+++ warmupExercises/1_template_replacement.php
@@ -5,14 +5,24 @@ declare(strict_types=1);
 /*
 1. Pakeiskite šablone tarp dvigubų riestinių skliaustų esančius kintamuosius į $replacements masyve esančius duomenis
 ir išspausdinkite gautą tekstą.
-Turėtumėte išspausdinti: 'Quick fox jumps over the lazy dog'
 Jeigu kažkuris šablone esantis kintamasis lieka nepakeistas, išspausdinkite klaidos pranešimą: 'Missing replacement variables'.
 Nebūtina naudoti objektinį programamvimą.
+
+Kodo kvietimo pavyzdys:
+replaceTemplate($replacements, $template);
+Quick fox jumps over the lazy dog
 */
 
-$template = 'Quick {{animal}} jumps over the {{obstacle_type}} {{obstacle}}{{sentence_end}}';
+$template = 'Quick {{animal}} jumps over the {{obstacle_type}} {{obstacle}} {{tesntrestnirsti}}';
 $replacements = [
     'animal' => 'fox',
     'obstacle' => 'dog',
     'obstacle_type' => 'lazy'
 ];
+
+function replaceTemplate(string $template, array $variables): void
+{
+    echo '';
+}
+
+replaceTemplate($template, $replacements);
